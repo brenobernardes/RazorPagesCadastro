dca32ad7537ca8e5cb5d855d5a7f203c232196ef
Breno Bernardes <breno.pbernardes@gmail.com>
2023-08-04 23:35:59 -0300

    hello world

---
+++ RazorPagesCadastro/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RazorPagesCadastro.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
