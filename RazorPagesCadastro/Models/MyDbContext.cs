4d818fcd5410910b417b16f29db859a959b20b96
Breno Bernardes <breno.pbernardes@gmail.com>
2023-08-05 22:35:55 -0300

    db context created

---
+++ RazorPagesCadastro/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesCadastro.Models
+{
+	public class MyDbContext : DbContext
+	{
+		public DbSet<Teacher> Teachers { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseInMemoryDatabase("MyDb");
+		}
+	}
+}
+
