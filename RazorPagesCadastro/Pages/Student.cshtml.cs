7f3ec533ad3ac8464da90e15a263feaf069a6a48
Breno Bernardes <breno.pbernardes@gmail.com>
2023-08-06 19:30:52 -0300

    adding some bootstrap

--- RazorPagesCadastro/Pages/Student.cshtml.cs
+++ RazorPagesCadastro/Pages/Student.cshtml.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesCadastro.Models;
 
 namespace RazorPagesCadastro.Pages
 {
 	public class StudentModel : PageModel
     {
+        private readonly MyDbContext _context;
+
+        public List<Student> Students { get; set; } = new List<Student>();
+
+        [BindProperty]
+        public required Student NewStudent { get; set; }
+
+        public StudentModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
         public void OnGet()
         {
+            Students = _context.Students.ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            _context.Students.Add(NewStudent);
+            _context.SaveChanges();
+            return RedirectToPage();
         }
     }
 }
