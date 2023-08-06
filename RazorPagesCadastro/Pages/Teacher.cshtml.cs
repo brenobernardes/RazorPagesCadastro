dbd3a1e05559343b3a2a4c3391383fbc764fe580
Breno Bernardes <breno.pbernardes@gmail.com>
2023-08-05 22:38:18 -0300

    pages teacher and student created

---
+++ RazorPagesCadastro/Pages/Teacher.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesCadastro.Models;
+
+namespace RazorPagesCadastro.Pages
+{
+	public class TeacherModel : PageModel
+    {
+        private readonly MyDbContext _context;
+
+        public List<Teacher> Teachers { get; set; } = new List<Teacher>();
+
+        [BindProperty]
+        public required Teacher NewTeacher { get; set; }
+
+        public TeacherModel(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+            Teachers = _context.Teachers.ToList();
+        }
+
+        public IActionResult OnPost()
+        {
+            _context.Teachers.Add(NewTeacher);
+            _context.SaveChanges();
+            return RedirectToPage();
+        }
+    }
+}
