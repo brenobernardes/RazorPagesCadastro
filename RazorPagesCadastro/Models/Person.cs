da050eda853a80628a54b9e914d16af66732c67d
Breno Bernardes <breno.pbernardes@gmail.com>
2023-08-06 22:02:32 -0300

    adding auto increment for the database

--- RazorPagesCadastro/Models/Person.cs
+++ RazorPagesCadastro/Models/Person.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace RazorPagesCadastro.Models
 {
     public class Person
     {
+        [Key]
         public int Id { get; set; }
         public required string FirstName { get; set; }
         public string? LastName { get; set; }
